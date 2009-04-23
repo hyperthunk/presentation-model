d0eeeeaee486f66578536152a223493a0e2d76f9
pax <watson.timothy@gmail.com>
2009-04-23 11:16:12 +0100

    setting up django-api Resource

--- django-api.js
+++ django-api.js
@@ -1,5 +1,30 @@
 
 // override some of the based
 var DjangoResource = Module.create(Resource, {
-    
+    id: function() {
+        return this.pk;
+    },
+    toJSON: function() {
+        var json = new Hash();
+        var self = this;
+        if (this.inline_fields.size() > 0) {
+            var fields = new Hash();
+            this.inline_fields.each(function(f) { fields.set(f, self[f]); });
+            json.set('fields', fields.toObject());
+        }
+        this.serializable.reject(function(s) {
+            return self.inline_fields.member(s);
+        } ).each(function(e) {
+            json.set(e, self[e]);
+        });
+        return json.toObject();
+    },
+    hydrate: function(json) {
+        // TODO: add support for nested Resource objects - maybe an override is required?
+        var data = $H(json);
+        fields = data.unset('fields');
+        data.update(fields || {});
+        this.inline_fields = Object.keys(fields);
+        return Resource.hydrate.apply(this, [data]);
+    }
 });
